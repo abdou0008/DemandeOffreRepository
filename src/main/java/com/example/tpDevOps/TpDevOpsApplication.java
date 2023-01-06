49cb15c5e0833cac119238ed7e0a54dc4cf5d73b
Mohamed <hp@Mohamed08.com>
2023-01-06 23:58:25 +0000

    first commit

---
+++ src/main/java/com/example/tpDevOps/TpDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpDevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpDevOpsApplication.class, args);
+	}
+
+}
